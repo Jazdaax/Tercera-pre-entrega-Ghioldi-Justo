1d61ec05b4465d06c927c2751bd99faf38b0fd6f
Jazdaax <justoghioldi23mail.com>
2024-06-05 21:53:52 -0300

    se personalizo el admin

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Curso, Profesores, Estudiantes, Entregable
+
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ['nombre',"camada"]
+    search_fields = ['nombre',"camada"]
+    list_filter = ['nombre']
 
 # Register your models here.
+admin.site.register(Curso)
+admin.site.register(Profesores)
+admin.site.register(Estudiantes)
+admin.site.register(Entregable)
--- AppCoder/models.py
+++ AppCoder/models.py
@@ -5,14 +5,35 @@ from django.db import models
 class Curso(models.Model):
     nombre = models.CharField(max_length=100)
     camada = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.camada}"
+    
+    class Meta():
+        verbose_name = "Curso"
+        verbose_name_plural = "Cursos"
+        ordering = ("nombre","camada")
+        unique_together = ("nombre","camada")
     
     
 class Profesores(models.Model):
     camada = models.IntegerField(Curso)  
     nombre =models.CharField(max_length=40)
     apellido = models.CharField(max_length=40)
-    mail = models.CharField(max_length=40)
+    mail = models.EmailField(null=True)
+    profesion = models.CharField(max_length=30, null=True)
+    cursos = models.ManyToManyField(Curso)
 
 class Estudiantes(models.Model):
     nombre = models.CharField(max_length=40)
     apellido = models.CharField(max_length=40)
+    mail = models.EmailField(null=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=100)
+    fechaDeEntrega = models.DateField()
+    entregado = models.BooleanField()
+    estudiante = models.ForeignKey(Estudiantes, on_delete=models.CASCADE)
